5e98829060b1f1ebfa8330f220212d012b321c63
jules <102880647+jupi-ter@users.noreply.github.com>
2025-10-20 10:42:00 -0300

    feat: ship moves.
    todo: must move bullets in the same way

--- main.lua
+++ main.lua
@@ -12,46 +12,71 @@ function new_player(xx, yy)
     local p = {
         x = xx,
         y = yy,
-        spd = 120, --this is pixels per second 120/60 || (120*0.0167) = 2px per button press
+        vx = 0,
+        vy = 0,
+        accel = 0,
+        accel_delta = 200,
+        max_accel = 300,  --this is pixels per second 120/60 || (120*0.0167) = 2px per button press
+        friction = 0.99,
         sprite = nil,
         angle_increment = 300, -- 5px
         shooting_counter = 0,
-        rotation_deg = 0
+        rotation_deg = 0,
+        shooting_cooldown = 60
     }
 
-    function p:update(dt)
-        -- decrement shooting counter
+    function p:decrement_shoot_cooldown()
+    -- decrement shooting counter
         if self.shooting_counter > 0 then
             self.shooting_counter = self.shooting_counter - 1
             if self.shooting_counter < 0 then
-                self.shooting_counter = 0
+            self.shooting_counter = 0
             end
         end
+    end
+
+    function p:init()
+        self.sprite = player_center_sprite
+    end
+
+    function p:update(dt)
+        p:decrement_shoot_cooldown()
 
         local move_left = love.keyboard.isDown("left")
         local move_right = love.keyboard.isDown("right")
         local move_up = love.keyboard.isDown("up")
 
-        --rotate
+        -- rotate
         if move_left then
-            self.x = self.x - self.spd * dt
+            self.rotation_deg = self.rotation_deg - (self.angle_increment * dt)
         end
 
         if move_right then
-            self.x = self.x + self.spd * dt
+            self.rotation_deg = self.rotation_deg + (self.angle_increment * dt)
         end
 
-        --accelerate
-        if move_up then self.y = self.y - self.spd * dt end
+        if move_up then
+            local rotation_rad = math.rad(self.rotation_deg)
+            self.vx = self.vx + math.cos(rotation_rad) * self.accel_delta * dt
+            self.vy = self.vy + math.sin(rotation_rad) * self.accel_delta * dt
+        end
+
+        -- apply friction / drag
+        self.vx = self.vx * self.friction
+        self.vy = self.vy * self.friction
+
+        -- move
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
 
         if love.keyboard.isDown("space") and self.shooting_counter == 0 then
-            self.shooting_counter = 60
+            self.shooting_counter = self.shooting_cooldown
 
-            local b = new_pbullet(self.x, self.y)
+            local b = new_pbullet(self.x, self.y, self.rotation_deg)
             table.insert(bullets, b)
         end
 
-        -- screen clamping
+        -- todo: screen wrapping
         if self.x > 128 then self.x = 128 end
         if self.x < 0 then self.x = 0 end
         if self.y < 0 then self.y = 0 end
@@ -68,14 +93,14 @@ function new_player(xx, yy)
 end
 
 --player bullet
-function new_pbullet(xx, yy)
+function new_pbullet(xx, yy, angle_deg)
 	local bul = {
 		x = xx,
 		y = yy,
 		spd = 480,
 		sprite = nil,
         run_once = false,
-        rotation = 0.0
+        rotation = angle_deg
 	}
 
     function bul:init()
@@ -90,8 +115,6 @@ function new_pbullet(xx, yy)
 
 	function bul:update(dt)
         self:init()
-    
-		self.y = self.y - self.spd * dt
 	end
 
 	function bul:draw()
@@ -109,7 +132,7 @@ function love.load()
     local width, height = 128, 128
 
     love.window.setMode(width, height, {
-        fullscreen = true,
+        fullscreen = false,
         resizable = true,
         vsync = true,
         minwidth = 128,
@@ -128,6 +151,8 @@ function love.load()
 
     --objects
     player = new_player(64,64)
+    player:init()
+
     bullets = {}
 end
 
